bf005b0f2cb5f132698715cebe07b107bc6b22fd
Ian Seyler <ian@ians-macbook-air.local>
2022-11-20 21:13:33 +0000

    Add peek function

--- src/monitor.asm
+++ src/monitor.asm
@@ -111,7 +111,15 @@ poll:
 	mov rdi, temp_string
 	mov rcx, 100
 	call input
-	; TODO clear leading/trailing spaces to sanitize input
+	jrcxz poll			; input stores the number of characters received in RCX
+	mov rsi, rdi
+	call string_parse		; Remove extra spaces
+	jrcxz poll			; string_parse stores the number of words in RCX
+	mov byte [args], cl		; Store the number of words in the string
+	; Break the contents of temp_string into individual strings
+	mov al, 0x20
+	xor bl, bl
+	call string_change_char
 
 	mov rsi, command_exec
 	call string_compare
@@ -129,12 +137,18 @@ poll:
 	call string_compare
 	jc load
 
+	mov rsi, command_peek
+	call string_compare
+	jc peek
+
+	mov rsi, command_poke
+	call string_compare
+	jc poke
+
 	mov rsi, command_help
 	call string_compare
 	jc help
 
-	cmp rcx, 0			; If no characters were entered show prompt again
-	je poll
 	mov rsi, message_unknown
 	call output
 	jmp poll
@@ -220,14 +234,85 @@ load:
 	; TODO
 	; load to memory, use RAX for starting sector
 	mov rdi, 0x200000
-	mov rcx, 1			; Loading 4K for now
+	mov rcx, 16			; Loading 64K for now
 	mov rdx, 0
 	call [b_storage_read]
 
 	jmp poll
 
+peek:
+	cmp byte [args], 3
+	jl insuf
+	jg toomany
+	
+	mov rsi, temp_string
+	call string_length
+	add rsi, 1
+	add rsi, rcx
+	;call output
+	call hex_string_to_int		; RAX holds the address
+	mov rbx, rax			; Save it to RBX
+
+	call string_length
+	add rsi, 1
+	add rsi, rcx
+	call hex_string_to_int		; RAX holds the bytes
+	cmp al, 1
+	je peek_b
+	cmp al, 2
+	je peek_w
+	cmp al, 4
+	je peek_d
+	cmp al, 8
+	je peek_q
+	mov rsi, invalidargs
+	call output
+	jmp poll
+
+peek_b:
+	mov rsi, rbx
+	lodsb
+	call dump_al
+	jmp peek_end
+
+peek_w:
+	mov rsi, rbx
+	lodsw
+	call dump_ax
+	jmp peek_end
+
+peek_d:
+	mov rsi, rbx
+	lodsd
+	call dump_eax
+	jmp peek_end
+
+peek_q:
+	mov rsi, rbx
+	lodsq
+	call dump_rax
+	jmp peek_end
+
+peek_end:
+	mov rsi, newline
+	call output
+	jmp poll
+
+poke:
+	jmp poll
+
 help:
 	mov rsi, message_help
+	call output
+	jmp poll
+
+insuf:
+	mov rsi, insufargs
+	call output
+	jmp poll
+
+toomany:
+	mov rsi, toomanyargs
 	call output
 	jmp poll
 
@@ -242,6 +327,8 @@ command_exec:		db 'exec', 0
 command_dir:		db 'dir', 0
 command_ver:		db 'ver', 0
 command_load:		db 'load', 0
+command_peek:		db 'peek', 0
+command_poke:		db 'poke', 0
 command_help:		db 'help', 0
 cpumsg:			db '[cpu: ', 0
 memmsg:			db ']  [mem: ', 0
@@ -255,6 +342,9 @@ closebracketmsg:	db ']', 0
 space:			db ' ', 0
 newline:		db 13, 0
 tab:			db 9, 0
+insufargs:		db 'Insufficient argument(s)', 13, 0
+toomanyargs:		db 'Too many arguments', 13, 0
+invalidargs:		db 'Invalid argument(s)', 13, 0
 dirmsg:			db '#       Name            Size', 13, '-----------------------------', 13, 0
 
 ; Variables
@@ -272,6 +362,7 @@ Screen_Cols:		dw 0
 Screen_Cursor_Row:	dw 0
 Screen_Cursor_Col:	dw 0
 VideoDepth:		db 0
+args:			db 0
 
 
 ; -----------------------------------------------------------------------------
@@ -793,6 +884,146 @@ string_compare_terminated:
 
 
 ; -----------------------------------------------------------------------------
+; string_chomp -- Strip leading and trailing spaces from a string
+;  IN:	RSI = string location
+; OUT:	All registers preserved
+string_chomp:
+	push rsi
+	push rdi
+	push rcx
+	push rax
+
+	call string_length		; Quick check to see if there are any characters in the string
+	jrcxz string_chomp_done	; No need to work on it if there is no data
+
+	mov rdi, rsi			; RDI will point to the start of the string...
+	push rdi			; ...while RSI will point to the "actual" start (without the spaces)
+	add rdi, rcx			; os_string_length stored the length in RCX
+
+string_chomp_findend:		; we start at the end of the string and move backwards until we don't find a space
+	dec rdi
+	cmp rsi, rdi			; Check to make sure we are not reading backward past the string start
+	jg string_chomp_fail		; If so then fail (string only contained spaces)
+	cmp byte [rdi], ' '
+	je string_chomp_findend
+
+	inc rdi				; we found the real end of the string so null terminate it
+	mov byte [rdi], 0x00
+	pop rdi
+
+string_chomp_start_count:		; read through string until we find a non-space character
+	cmp byte [rsi], ' '
+	jne string_chomp_copy
+	inc rsi
+	jmp string_chomp_start_count
+
+string_chomp_fail:			; In this situation the string is all spaces
+	pop rdi				; We are about to bail out so make sure the stack is sane
+	xor al, al
+	stosb
+	jmp string_chomp_done
+
+; At this point RSI points to the actual start of the string (minus the leading spaces, if any)
+; And RDI point to the start of the string
+
+string_chomp_copy:		; Copy a byte from RSI to RDI one byte at a time until we find a NULL
+	lodsb
+	stosb
+	test al, al
+	jnz string_chomp_copy
+
+string_chomp_done:
+	pop rax
+	pop rcx
+	pop rdi
+	pop rsi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; string_parse -- Parse a string into individual words
+;  IN:	RSI = Address of string
+; OUT:	RCX = word count
+; Note:	This function will remove "extra" white-space in the source string
+;	"This is  a test. " will update to "This is a test."
+string_parse:
+	push rsi
+	push rdi
+	push rax
+
+	xor ecx, ecx			; RCX is our word counter
+	mov rdi, rsi
+
+	call string_chomp		; Remove leading and trailing spaces
+
+	cmp byte [rsi], 0x00		; Check the first byte
+	je string_parse_done		; If it is a null then bail out
+	inc rcx				; At this point we know we have at least one word
+
+string_parse_next_char:
+	lodsb
+	stosb
+	test al, al			; Check if we are at the end
+	jz string_parse_done		; If so then bail out
+	cmp al, ' '			; Is it a space?
+	je string_parse_found_a_space
+	jmp string_parse_next_char	; If not then grab the next char
+
+string_parse_found_a_space:
+	lodsb				; We found a space.. grab the next char
+	cmp al, ' '			; Is it a space as well?
+	jne string_parse_no_more_spaces
+	jmp string_parse_found_a_space
+
+string_parse_no_more_spaces:
+	dec rsi				; Decrement so the next lodsb will read in the non-space
+	inc rcx
+	jmp string_parse_next_char
+
+string_parse_done:
+	pop rax
+	pop rdi
+	pop rsi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; string_change_char -- Change all instances of a character in a string
+;  IN:	RSI = string location
+;	AL  = character to replace
+;	BL  = replacement character
+; OUT:	All registers preserved
+string_change_char:
+	push rsi
+	push rcx
+	push rbx
+	push rax
+
+	mov cl, al
+string_change_char_loop:
+	mov byte al, [rsi]
+	test al, al
+	jz string_change_char_done
+	cmp al, cl
+	jne string_change_char_no_change
+	mov byte [rsi], bl
+
+string_change_char_no_change:
+	inc rsi
+	jmp string_change_char_loop
+
+string_change_char_done:
+	pop rax
+	pop rbx
+	pop rcx
+	pop rsi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
 ; string_from_int -- Convert a binary integer into an string
 ;  IN:	RAX = binary integer
 ;	RDI = location to store string
@@ -869,8 +1100,98 @@ string_to_int_invalid:
 ; -----------------------------------------------------------------------------
 
 
+; -----------------------------------------------------------------------------
+; hex_string_to_int -- Convert up to 8 hexascii to bin
+;  IN:	RSI = Location of hex asciiz string
+; OUT:	RAX = binary value of hex string
+;	All other registers preserved
+hex_string_to_int:
+	push rsi
+	push rcx
+	push rbx
+
+	cld
+	xor ebx, ebx
+hex_string_to_int_loop:
+	lodsb
+	mov cl, 4
+	cmp al, 'a'
+	jb hex_string_to_int_ok
+	sub al, 0x20				; convert to upper case if alpha
+hex_string_to_int_ok:
+	sub al, '0'				; check if legal
+	jc hex_string_to_int_exit		; jump if out of range
+	cmp al, 9
+	jle hex_string_to_int_got		; jump if number is 0-9
+	sub al, 7				; convert to number from A-F or 10-15
+	cmp al, 15				; check if legal
+	ja hex_string_to_int_exit		; jump if illegal hex char
+hex_string_to_int_got:
+	shl rbx, cl
+	or bl, al
+	jmp hex_string_to_int_loop
+hex_string_to_int_exit:
+	mov rax, rbx				; integer value stored in RBX, move to RAX
+
+	pop rbx
+	pop rcx
+	pop rsi
+	ret
+; -----------------------------------------------------------------------------
+
+
+; -----------------------------------------------------------------------------
+; dump_(rax|eax|ax|al) -- Dump content of RAX, EAX, AX, or AL
+;  IN:	RAX = content to dump
+; OUT:	Nothing, all registers preserved
+dump_rax:
+	rol rax, 8
+	call dump_al
+	rol rax, 8
+	call dump_al
+	rol rax, 8
+	call dump_al
+	rol rax, 8
+	call dump_al
+	rol rax, 32
+dump_eax:
+	rol eax, 8
+	call dump_al
+	rol eax, 8
+	call dump_al
+	rol eax, 16
+dump_ax:
+	rol ax, 8
+	call dump_al
+	rol ax, 8
+dump_al:
+	push rbx
+	push rax
+	mov rbx, hextable
+	push rax			; Save RAX since we work in 2 parts
+	shr al, 4			; Shift high 4 bits into low 4 bits
+	xlatb
+	mov [tchar+0], al
+	pop rax
+	and al, 0x0f			; Clear the high 4 bits
+	xlatb
+	mov [tchar+1], al
+	push rsi
+	push rcx
+	mov rsi, tchar
+	call output
+	pop rcx
+	pop rsi
+	pop rax
+	pop rbx
+	ret
+; -----------------------------------------------------------------------------
+
+
 %include 'font.inc'
 
+hextable: db '0123456789ABCDEF'
+tchar: db 0, 0, 0
 temp_string1: times 50 db 0
 temp_string2: times 50 db 0
 temp_string: db 0
