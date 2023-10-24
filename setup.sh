aa1e0c1c94e6f953e7dc24f7b3a2135d09892595
Ian Seyler <ian@ians-macbook-air.local>
2023-10-24 15:21:12 -0400

    Update setup to use curl or wget

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@
 
 mkdir src/api
 cd src/api
-curl -s -o libBareMetal.asm https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+if which curl &> /dev/null; then
+	curl -s -o libBareMetal.asm https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+else
+	wget https://raw.githubusercontent.com/ReturnInfinity/BareMetal/master/api/libBareMetal.asm
+fi
 cd ../..
 
 mkdir bin
