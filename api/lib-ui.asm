f0fe3eb263e62cbcce76c61d03ad620aa890e30e
isoux <isa@isoux.org>
2024-11-27 11:36:20 +0200

    Lib ui

---
+++ api/lib-ui.asm
@@ -0,0 +1,25 @@
+; =============================================================================
+; BareMetal Monitor UI
+; Copyright (C) 2008-2024 Return Infinity -- see LICENSE.TXT
+;
+; Version 1.0
+; =============================================================================
+
+; Index for ui_api -- API calls for the UI
+RETURN      equ 0x00
+GET_FG      equ 0x01
+GET_BG      equ 0x02
+GET_CURSOR_ROW  equ 0x03
+GET_CURSOR_COL  equ 0x04
+GET_CURSOR_ROW_MAX  equ 0x05
+GET_CURSOR_COL_MAX  equ 0x06
+SET_FG equ equ  0x11
+SET_BG equ equ  0x12
+SET_CURSOR_ROW  equ 0x13
+SET_CURSOR_COL  equ 0x14
+SET_CURSOR_ROW_MAX  equ 0x15
+SET_CURSOR_COL_MAX  equ 0x16
+
+
+; =============================================================================
+; EOF
