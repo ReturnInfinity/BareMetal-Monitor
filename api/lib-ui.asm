327384d5b03851018502dc890a6ace2d2ab74564
isoux <isa@isoux.org>
2024-11-27 23:40:19 +0200

    Update lib-ui

--- api/lib-ui.asm
+++ api/lib-ui.asm
@@ -9,14 +9,14 @@
 RETURN      equ 0x00
 GET_FG      equ 0x01
 GET_BG      equ 0x02
-GET_CURSOR_ROW  equ 0x03
-GET_CURSOR_COL  equ 0x04
+GET_CURSOR_ROW      equ 0x03
+GET_CURSOR_COL      equ 0x04
 GET_CURSOR_ROW_MAX  equ 0x05
 GET_CURSOR_COL_MAX  equ 0x06
-SET_FG equ equ  0x11
-SET_BG equ equ  0x12
-SET_CURSOR_ROW  equ 0x13
-SET_CURSOR_COL  equ 0x14
+SET_FG      equ  0x11
+SET_BG      equ  0x12
+SET_CURSOR_ROW      equ 0x13
+SET_CURSOR_COL      equ 0x14
 SET_CURSOR_ROW_MAX  equ 0x15
 SET_CURSOR_COL_MAX  equ 0x16
 
---
+++ api/lib-ui.h
@@ -0,0 +1,31 @@
+// =============================================================================
+// BareMetal -- a 64-bit OS written in Assembly for x86-64 systems
+// Copyright (C) 2008-2024 Return Infinity -- see LICENSE.TXT
+//
+// Version 1.0
+// =============================================================================
+
+// Headers
+#ifndef _LIBUI_H
+#define _LIBUI_H
+
+// Index for ui_api -- API calls for the UI
+#define RETURN      0x00
+#define GET_FG      0x01
+#define GET_BG      0x02
+#define GET_CURSOR_ROW      0x03
+#define GET_CURSOR_COL      0x04
+#define GET_CURSOR_ROW_MAX  0x05
+#define GET_CURSOR_COL_MAX  0x06
+#define SET_FG      0x11
+#define SET_BG      0x12
+#define SET_CURSOR_ROW      0x13
+#define SET_CURSOR_COL      0x14
+#define SET_CURSOR_ROW_MAX  0x15
+#define SET_CURSOR_COL_MAX  0x16
+
+#endif
+
+
+// =============================================================================
+// EOF
