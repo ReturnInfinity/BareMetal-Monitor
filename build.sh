63af7fac0ed1c499e5e54d344c990ca68d31e5af
Ian Seyler <ian@ians-mac-mini.local>
2024-12-23 08:35:42 -0500

    Cross-platform shebang

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd src
 nasm monitor.asm -o ../bin/monitor.bin -l ../bin/monitor-debug.txt
--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 rm -rf src/api/
 rm -rf bin/
